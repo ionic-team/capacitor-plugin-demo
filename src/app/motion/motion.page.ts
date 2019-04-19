7da2e422bd27d977fc4f5fa0f5cea032de3ded9e
Ken Sodemann <ken@ionic.io>
2019-04-19 07:28:57 -0500

    WIP - android project updates

--- src/app/motion/motion.page.ts
+++ src/app/motion/motion.page.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-motion',
   templateUrl: './motion.page.html',
-  styleUrls: ['./motion.page.scss'],
+  styleUrls: ['./motion.page.scss']
 })
-export class MotionPage implements OnInit {
-
-  constructor() { }
+export class MotionPage implements OnDestroy, OnInit {
+  acceleration: { x: number; y: number; z: number };
+  accelerationIncludingGravity: { x: number; y: number; z: number };
+  interval: number;
+  rotationRate: { alpha: number; beta: number; gamma: number };
+  orientaion: { alpha: number; beta: number; gamma: number };
 
   ngOnInit() {
+    Plugins.Motion.addListener('accel', evt => {
+      this.acceleration = evt.acceleration;
+      this.accelerationIncludingGravity = evt.accelerationIncludingGravity;
+      this.interval = evt.interval;
+      this.rotationRate = evt.rotationRate;
+    });
+
+    Plugins.Motion.addListener('orientation', d => {
+      this.orientaion = d;
+    });
   }
 
+  ngOnDestroy() {}
 }
